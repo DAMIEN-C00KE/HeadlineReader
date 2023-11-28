d1f142cfe762273e59616c383a240e6a9bfbc0fd
HCT <110128281+DAMIEN-C00KE@users.noreply.github.com>
2023-11-28 12:41:18 +0200

    Create main.jl

---
+++ main.jl
@@ -0,0 +1,48 @@
+using HTTP, Gumbo, Cascadia, SQLite, Dates
+
+function fetch_headlines(url)
+    response = HTTP.get(url)
+    parsed_html = parsehtml(String(response.body))
+
+    headlines = []
+    for tag in ["h1", "h2", "h3"]
+        elements = eachmatch(Selector(tag), parsed_html.root)
+        for elem in elements
+            push!(headlines, Gumbo.text(elem))
+        end
+    end
+
+    return headlines
+end
+
+function log_headlines_to_db(headlines, url, db)
+    for headline in headlines
+        SQLite.execute(db, "INSERT INTO headlines (date, url, headline) VALUES (?, ?, ?)",
+                        (Dates.now(), url, headline))
+    end
+end
+
+function main()
+    urls = ["https://cointelegraph.com/", "https://www.coindesk.com/"]
+    db = SQLite.DB("headlines.db")
+
+    SQLite.execute(db, """
+        CREATE TABLE IF NOT EXISTS headlines (
+            id INTEGER PRIMARY KEY,
+            date TEXT,
+            url TEXT,
+            headline TEXT
+        )
+    """)
+
+    while true
+        for url in urls
+            headlines = fetch_headlines(url)
+            log_headlines_to_db(headlines, url, db)
+        end
+
+        sleep(60 * 5) # Adjust as needed
+    end
+end
+
+main()
