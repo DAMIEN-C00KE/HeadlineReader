0bac2fe20611ac0646fcf4a86e41f57510e1618e
HCT <110128281+DAMIEN-C00KE@users.noreply.github.com>
2023-11-30 13:22:28 +0200

    Update main.jl
    Added word count feature

--- main.jl
+++ main.jl
@@ -1,7 +1,11 @@
 # Import packages required
 using HTTP, Gumbo, Cascadia, SQLite, Dates
 
-# Web request
+# Define stop words
+stop_words = Set(["the", "is", "at", "of", "and", "in", "to", "a", "crypto", "cryptocurrency", "news", "with", "as",
+"for", "s", "u", "1", "on", "that", "this", "its", "it"])
+
+# Function to fetch headlines
 function fetch_headlines(url)
     response = HTTP.get(url)
     parsed_html = parsehtml(String(response.body))
@@ -17,18 +21,44 @@ function fetch_headlines(url)
     return headlines
 end
 
-# Log headlines to SQLite database
+# Log headlines to database
 function log_headlines_to_db(headlines, url, db)
     for headline in headlines
+        # Format the current date-time as text
+        datetime_text = Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS")
+
         SQLite.execute(db, "INSERT INTO headlines (date, url, headline) VALUES (?, ?, ?)",
-                        (Dates.now(), url, headline))
+                        (datetime_text, url, headline))
+    end
+end
+
+# Count words
+function count_words(headlines)
+    word_counts = Dict{String, Int}()
+    for headline in headlines
+        words = split(lowercase(headline), r"\W+")
+        for word in words
+            if word ∉ stop_words && word != ""
+                word_counts[word] = get(word_counts, word, 0) + 1
+            end
+        end
+    end
+    return word_counts
+end
+
+# Log word counts to database
+function log_word_counts_to_db(word_counts, db)
+    for (word, count) in word_counts
+        SQLite.execute(db, "INSERT into word_counts (word, count) VALUES (?, ?)", 
+                        (word, count))
     end
 end
 
 function main()
-    urls = [] # Input URLs (comma seperated)
+    urls = ["example_url.com", "example_url2.com"] # Input URLs (comma seperated)
     db = SQLite.DB("headlines.db")
 
+    # Create tables if they don't exist
     SQLite.execute(db, """
         CREATE TABLE IF NOT EXISTS headlines (
             id INTEGER PRIMARY KEY,
@@ -38,10 +68,21 @@ function main()
         )
     """)
 
+    SQLite.execute(db, """
+        CREATE TABLE IF NOT EXISTS word_counts (
+            id INTEGER PRIMARY KEY,
+            word TEXT,
+            count INTEGER
+        )
+    """)
+
     while true
         for url in urls
             headlines = fetch_headlines(url)
             log_headlines_to_db(headlines, url, db)
+
+            word_counts = count_words(headlines)
+            log_word_counts_to_db(word_counts, db)
         end
 
         sleep(60 * 5) # Adjust as needed
